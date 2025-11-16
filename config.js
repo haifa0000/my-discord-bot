a13f048ea8def497d49a2a4ddec9342d3ed9bfb4
haifa0000 <134120650+haifa0000@users.noreply.github.com>
2025-11-16 05:26:17 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-// هذا الملف آمن ولا يحتوي أي توكن أو IDs مباشرة
-// يقوم فقط بقراءة القيم من متغيرات البيئة (Environment Variables)
-
-module.exports = {
-  DISCORD_TOKEN: process.env.DISCORD_TOKEN,
-  CLIENT_ID: process.env.CLIENT_ID,
-  GUILD_ID: process.env.GUILD_ID
-};
